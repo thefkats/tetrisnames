af093ed63f1f9da3fd7740726a9c104ee8a2a0c1
James Fkat <thefkats@gmail.com>
2017-02-03 15:51:59 -0600

    Added more functionality and started putting in formfiller

--- src/register/Account.java
+++ src/register/Account.java
@@ -1,10 +1,27 @@
 package register;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import captcha.Cracker;
 
 public class Account {
 
@@ -30,12 +47,15 @@ public class Account {
 		this.email = email;
 	}
 
-	public void register() {
-		// TODO
+	public boolean register() {
+		isRegistered = register(username, email, password);
+		
+		return false;
 	}
 
-	public void validate() {
+	public boolean validate() {
 		// TODO
+		return false;
 	}
 
 	public void play() {
@@ -70,6 +90,10 @@ public class Account {
 		return username.replaceAll("_", ".u") + "@grr.la";
 	}
 
+	public Account clone() {
+		return new Account(username, password, email);
+	}
+	
 	public static boolean isValidated(String userName, String passWord) {
 		// TODO
 		return false;
@@ -106,6 +130,76 @@ public class Account {
 
 		}
 		System.out.println("[error: serious] Unchecked username in Account.userName");
+		return false;
+	}
+	
+	private boolean register(String uname, String pword, String eMail) {
+		String exePath = "chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.tetrisfriends.com/users/register.php?saveGame=true&linkTag=nav");
+		
+		WebElement email = driver.findElement(By.id("email"));
+		WebElement uName = driver.findElement(By.id("username"));
+		WebElement pWord1 = driver.findElement(By.id("password"));
+		WebElement pWord2 = driver.findElement(By.id("password_dup"));
+		Select month = new Select(driver.findElement(By.id("month")));
+		Select day = new Select(driver.findElement(By.id("day")));
+		Select year = new Select(driver.findElement(By.id("year")));
+		WebElement pBDay = driver.findElement(By.name("is_private_birth_date"));
+		Select country = new Select(driver.findElement(By.name("country")));
+		WebElement pLoc = driver.findElement(By.name("is_private_location"));
+		Select state = new Select(driver.findElement(By.id("stateSelect")));
+
+		WebElement img = driver.findElement(By.name("captcha_img"));
+
+		File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		// FileUtils.copyFile(screen, new
+		// File("C:/Users/James/Documents/programming/java/tfregister/captchas/toBeSorted/unsplit/temp2.png"));
+
+		int width = img.getSize().getWidth();
+		int height = img.getSize().getHeight();
+		BufferedImage bImg = ImageIO.read(screen);
+
+		// System.out.println(img.getLocation().getX() + "," +
+		// img.getLocation().getY());
+		// BufferedImage dest = bImg.getSubimage(img.getLocation().getX(),
+		// img.getLocation().getY(), width, height);
+		BufferedImage dest = bImg.getSubimage(315, 542, 250, 62);
+
+		int w = dest.getWidth();
+		int h = dest.getHeight();
+		BufferedImage after = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		AffineTransform at = new AffineTransform();
+		at.scale(0.8, 0.8);
+		AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+		after = scaleOp.filter(dest, after);
+
+		ImageIO.write(after, "png", screen);
+		File file = new File(path + "test.jpg");
+		FileUtils.copyFile(screen, file);
+		// File file2 = new File(path + "omg.png");
+		// FileUtils.copyFile(screen, file2);
+
+		KeyCracker c = new KeyCracker("test.jpg");
+		String keyText = c.getKey();
+
+		WebElement key = driver.findElement(By.name("captcha"));
+
+		email.sendKeys("email@emaail.com");
+		uName.sendKeys("a19487");
+		pWord1.sendKeys("asdf123p");
+		pWord2.sendKeys("asdfpass");
+		month.selectByValue("7");
+		day.selectByValue("3");
+		year.selectByValue("1997");
+		country.selectByValue("US");
+
+		state.selectByValue("NJ");
+		pBDay.click();
+		pLoc.click();
+		key.sendKeys(keyText);
+		
 		return false;
 	}
 }
--- src/register/Main.java
+++ src/register/Main.java
@@ -1,21 +1,117 @@
 package register;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	private ArrayList<Account> accounts;
-	
+	private String path;
+	private int user;
+
 	public static void main(String[] args) {
+		Main m = new Main();
+		m.addAccount("test37");
+		m.registerAll();
+		System.out.println(m.getAccounts().size());
+		m.saveUsers();
+	}
+
+	public Main() {
+		setup("", 0);
+	}
+
+	public Main(String path) {
+		setup(path, 0);
+	}
+
+	public Main(int user) {
+		setup("", user);
+	}
+
+	private void setup(String path, int user) {
+		accounts = new ArrayList<>();
+		this.path = path;
+		this.user = user;
+		loadUsers();
+	}
+
+	public ArrayList<Account> getAccounts() {
+		ArrayList<Account> accountsCopy = new ArrayList<>();
+		for (Account a : accounts) {
+			accountsCopy.add(a.clone());
+		}
 		
+		return accountsCopy;
+	}
+
+	public void addAccount(Account a) {
+		accounts.add(a);
+	}
+
+	public void addAccount(String uname) {
+		accounts.add(new Account(uname));
+	}
+
+	public boolean registerAll() {
+		boolean allRegistered = true;
+		for (Account a : accounts) {
+			if (!a.isRegistered())
+				if (!a.register()) {
+					System.out.println("[error: small] Unable to register name: " + a.getUsername());
+					allRegistered = false;
+				}
+		}
 		
+		return allRegistered;
 	}
 	
-	private void loadUsers() {
-		
+	private boolean loadUsers() {
+		File accountFile = new File(path + "accounts.txt");
+		if (!accountFile.exists())
+			return true;
+		try {
+			Scanner accountScan = new Scanner(accountFile);
+			while (accountScan.hasNextLine()) {
+				// TODO add in function that autofills out if line isn't filled
+				// (if only a username is there)
+				String uname = accountScan.nextLine();
+				accounts.add(new Account(uname));
+			}
+			accountScan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("[error: medium] Couldn't load file: " + path + "accounts.txt");
+			return false;
+		}
+		return true;
+
+		// TODO connect with website
 	}
-	
-	private void saveUsers() {
-		
+
+	private boolean saveUsers() {
+		File accountFile = new File(path + "accounts.txt");
+		try {
+			PrintWriter accountScan = new PrintWriter(accountFile);
+
+			Set<String> uniqueAccounts = new HashSet<>();
+			for (Account a : accounts) {
+				if (!uniqueAccounts.contains(a.getUsername())) {
+					accountScan.println(a.getUsername());
+					uniqueAccounts.add(a.getUsername());
+				}
+			}
+			accountScan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("[error: medium] Couldn't save in: " + path + "accounts.txt");
+			return false;
+		}
+		return true;
+
+		// TODO connect with website
 	}
-	
 }
